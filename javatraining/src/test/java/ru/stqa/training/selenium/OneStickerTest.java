a0feadeb12da29090e33fb5fbb115f73bceafeb2
esp1erre <symalnev@gmail.com>
2018-07-26 13:20:54 +0300

    + task 8 (checking product stickers on the main page)

---
+++ javatraining/src/test/java/ru/stqa/training/selenium/OneStickerTest.java
@@ -0,0 +1,39 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+public class OneStickerTest {
+    WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void testOneItemSticker() {
+        driver.get("http://localhost/litecart/en/");
+        List<WebElement> products = driver.findElements(By.xpath("//*[@class='listing-wrapper products']/li"));
+        for (WebElement product : products) {
+            int productStickerNumber = product.findElements(By.xpath(".//*[starts-with(@class, 'sticker ')]")).size();
+            assertEquals(String.format("Sticker number wrong for the product:\n%s ", product.getText()),
+                    1, productStickerNumber);
+        }
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
